843afbde8938db9d71a1b698f4cd3b09c80da83f
kyndaa <kynda@nashif.org>
2024-03-30 23:35:51 -0400

    coded isoprox

--- iso_proxg.m
+++ iso_proxg.m
@@ -1,10 +1,22 @@
-function [output] = iso_proxg(b,x, t, rho, gamma)
+function [output] = iso_proxg(y2,y3, t, gamma)
 %DESCRIPTION: Prox operator of the iso norm 
-%   INPUT:  b                = blurred image
+%   INPUT:  
 %           t                = stepsize
-%           rho              = relaxation parameter
-%           x                = input vector
+%           (y2,y3)          = input vector
 %           gamma            = de-noising parameter
-%   OUTPUT:  
+%   OUTPUT:  gamma*prox( iso(y2,y3) )
 %
+
+Y23 = [y2, y3]; 
+norm = [sqrt(Y23(:,1)^2+Y23(:,2)^2)]; %component-wise: sqrt(y1_k^2 + y2_k^2)
+logic = norm>t; %vector of 1s and 0s to represent the constraint of the prox
+
+%if norm > t: (y2_k y3_k) - t*(y2_k y3_k)/norm, aka divide each row [y2_k y3_k] by its norm
+
+%if otherwise: 0 (due to logic vector)
+
+%multiply in gamma bc this is prox of g(y)
+output = gamma*(logic.*(Y23 - t*Y23./norm) );
+
+
 end
