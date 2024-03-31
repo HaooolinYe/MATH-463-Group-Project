ba1215a7084a86438697a47613e0b1b927565620
kyndaa <kynda@nashif.org>
2024-03-30 21:44:51 -0400

    Update admm.m

--- admm.m
+++ admm.m
@@ -1,9 +1,9 @@
-function [x] = admm(b, t, rho, init_vectors, problem)
+function [x] = admm(b, t, rho, init_vectors, problem, i)
 %DESCRIPTION: Alternating Direction Method of Multipliers Algorithm
 %   INPUT:  b                = blurred image
 %           t                = stepsize
 %           p                = relaxation parameter
-%           init_vectors     = the initial vectors (x_0, u_0, y_0, w_0, z_0)
+%           init_vectors     = the initial vectors (u_0, y_0, w_0, z_0)
 %           problem          = specify the norm 
 %   OUTPUT: x                = "deblurred" image 
 %
