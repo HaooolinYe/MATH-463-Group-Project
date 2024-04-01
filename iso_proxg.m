2f21f7afa9ac71072873babf1d9bb425ef196843
Peter Bullard <peter.bullard@mail.mcgill.ca>
2024-04-01 16:47:03 -0400

    isoProx and proxl1 fixes

--- iso_proxg.m
+++ iso_proxg.m
@@ -8,7 +8,7 @@ function [output] = iso_proxg(y2,y3, t, gamma)
 %
 
 Y23 = [y2, y3]; 
-norm = [sqrt(Y23(:,1)^2+Y23(:,2)^2)]; %component-wise: sqrt(y1_k^2 + y2_k^2)
+norm = [sqrt(y2.^2+y3.^2)]; %component-wise: sqrt(y2_k^2 + y3_k^2)
 logic = norm>t; %vector of 1s and 0s to represent the constraint of the prox
 
 %if norm > t: (y2_k y3_k) - t*(y2_k y3_k)/norm, aka divide each row [y2_k y3_k] by its norm
