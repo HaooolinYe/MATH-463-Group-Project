79db5301b9a3aad4091fbdae87a00f6981dc45c9
kyndaa <kynda@nashif.org>
2024-04-01 21:24:11 -0400

    calculating full proxg
    function that outputs all of prox g (using l1/l2 and iso prox) outputs a 3nxn matrix

--- iso_proxg.m
+++ iso_proxg.m
@@ -1,22 +1,21 @@
-function [output] = iso_proxg(y2,y3, t, gamma)
+function [prox2,prox3] = iso_proxg(y2,y3, t, gamma)
 %DESCRIPTION: Prox operator of the iso norm 
 %   INPUT:  
 %           t                = stepsize
-%           (y2,y3)          = input vector
+%           y2,y3            = y(:,:,2) and y(:,:,3)
 %           gamma            = de-noising parameter
 %   OUTPUT:  gamma*prox( iso(y2,y3) )
-%
 
-Y23 = [y2, y3]; 
-norm = [sqrt(y2.^2+y3.^2)]; %component-wise: sqrt(y2_k^2 + y3_k^2)
-logic = norm>t; %vector of 1s and 0s to represent the constraint of the prox
 
-%if norm > t: (y2_k y3_k) - t*(y2_k y3_k)/norm, aka divide each row [y2_k y3_k] by its norm
+    %if norm > t: (y2_k y3_k) - t*(y2_k y3_k)/norm, 
+    % aka divide each row [y2_k y3_k] by its norm
+    %if otherwise: 0 (due to logic vector)
 
-%if otherwise: 0 (due to logic vector)
+    norm = (y2.^2+y3.^2).^(1/2); %component-wise: sqrt(y2_k^2 + y3_k^2)
+    logic = norm>t; %bool vect represents the constraint of the prox+
 
-%multiply in gamma bc this is prox of g(y)
-output = gamma*(logic.*(Y23 - t*Y23./norm) );
+    prox2 = gamma*(logic.*(y2 - t*y2./norm) ); 
+    prox3 = gamma*(logic.*(y3 - t*y3./norm) );
 
 
 end
--- norm_proxg.m
+++ norm_proxg.m
@@ -1,20 +1,26 @@
-function [output] = norm_proxg(norm, b,x, t, rho, gamma)
-%DESCRIPTION: Prox operator for l1 and l2 norms 
+function [y] = norm_proxg(norm, b,y, t, gamma)
+%DESCRIPTION: Prox operator on g(y). first we find proxl1/l2 for y1 then we
+%find iso prox for y2,y3. outputting the 3n x n matrix y(:,:,:) 
 %   INPUT:  norm             = problem (either l1 or l2)
-%           b                = blurred image
+%           b                = blurred image for l1/l2
 %           t                = stepsize
-%           rho              = relaxation parameter
-%           x                = input vector
-%           gamma            = de-noising parameter
-%   OUTPUT: l1 or l2 depending on parameter
-%
+%           y                = input matrix 3nxn (concatenated y1,y2,y3)
+%           gamma            = de-noising parameter for iso
+%   OUTPUT: y = prox(t,g)
+
+  
+    %finding l1 or l2 prox
     if norm == l1
         %insert l1 prox g
-        absxk = abs(x);
-        logic = absxk>t;
-        output = logic.*(sign(x).*(absxk-t));
+        absyk = abs(y(:,:,1));
+        logic = absyk>t;
+        y(:,:,1) = logic.*(sign(y(:,:,1)).*(absyk-t));
     else
         %insert l2 prox g
-        output = l2prox(x,b,t);
+        y(:,:,1) = l2prox(y(:,:,1),b,t);
     end
+
+    %find iso prox
+    [y(:,:,2), y(:,:,3)] = iso_proxg(y(:,:,2),y(:,:,3),t,gamma);
+
 end
