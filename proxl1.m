b1b1e75446bdb39362d16a2f5b5356c216284f55
Peter Bullard <peter.bullard@mail.mcgill.ca>
2024-04-04 14:22:27 -0400

    new proxl1

--- proxl1.m
+++ proxl1.m
@@ -1,12 +1,12 @@
-function [ output ] = proxl1(t, b, x)
+function [ output ] = proxl1(t, b, y1)
 %DESCRIPTION: Prox operator of the l1 norm 
 %   INPUT:  
 %           t                = stepsize
 %           b                = blurred image
 %           x                = input vector
 %   OUTPUT: prox operator of l1 norm (soft thresholding) applied to x
-    x = x-b;
-    absxk = abs(x);
+    z = b - y1;
+    absxk = abs(z);
     logic = absxk>t;
-    output = logic.*(sign(x).*(absxk-t));
+    output = logic.*(sign(z)*t+y1);
 end
