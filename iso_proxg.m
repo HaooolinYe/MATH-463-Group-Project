7a51ddd27ee364fee96f1d53e005d3b46e9a5c0b
kyndaa <kynda@nashif.org>
2024-04-03 21:20:34 -0400

    Update iso_proxg.m

--- iso_proxg.m
+++ iso_proxg.m
@@ -5,10 +5,17 @@ function [prox2,prox3] = iso_proxg(y2,y3, t, gamma)
 %           gamma            = de-noising parameter (step-size)
 %   OUTPUT:  prox( iso(y2,y3) )
 
- 
-    alpha = 1-gamma./max( (y2.^2+y3.^2).^(1/2) , gamma ); 
+    
+    if gamma == 0
+        %if gamma is 0 just output 0 (for debugging purposes)
+        prox2 = zeros(size(y2));
+        prox3 = zeros(size(y3));
+    else
 
-    prox2 = y2.*alpha;
-    prox3 = y3.*alpha;
+        %actual prox calculation
+        alpha = 1-gamma./max( (y2.^2+y3.^2).^(1/2) , gamma ); 
+    
+        prox2 = y2.*alpha;
+        prox3 = y3.*alpha;
 
-end
+    end
